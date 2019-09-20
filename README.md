aa658ef71b4d908c34bb6aab49e383ea8c33346d
Niklas Eicker <git@nikl.me>
2019-09-20 14:25:22 +0200

    refactor

--- README.md
+++ README.md
@@ -2,4 +2,17 @@
 
 Backend developing challenge for an interview process.
 
-Expose two endpoints allowing to create a new account for an existing user and to get user information.
+Task: expose two endpoints allowing to create a new account for an existing user and to get user information including accounts and transactions.
+
+## Running and testing
+A `.env` file can be created to change the port and allowed origin. 
+
+Start the service by executing
+```bash
+npm run start
+```
+
+Run the tests with
+```bash
+npm run test
+```
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 // CORS - allow origin from env file
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
+  res.header("Access-Control-Allow-Origin", process.env.ORIGIN || "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
@@ -16,6 +16,8 @@ app.use(function(req, res, next) {
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
+
+app.use(require('./routes'))
 
 app.listen(process.env.PORT || 3000, function() {
   console.log(`App running on port ${process.env.PORT || 3000}`);
