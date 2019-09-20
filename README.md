c44347c84d6cc9704e30aa9ceb7602c2e015323f
Niklas Eicker <git@nikl.me>
2019-09-20 10:49:52 +0200

    initial commit :rocket:

---
+++ README.md
@@ -0,0 +1,5 @@
+# German silver audi
+
+Backend developing challenge for an interview process.
+
+Expose two endpoints allowing to create a new account for an existing user and to get user information.
---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// CORS - allow origin from env file
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log(`App running on port ${process.env.PORT || 3000}`);
+});
